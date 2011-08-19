38e2cb1543b1b02cc08507445463b935e1ddbb7c
Jon Kaufman <jhk12345@gmail.com>
2011-08-19 14:44:44 -0400

    setup admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Equity::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "admin#home"
 
   # See how all your routes lay out with "rake routes"
 
