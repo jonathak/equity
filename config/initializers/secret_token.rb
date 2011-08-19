8930161f185c7451e395346ed61d1ed5f4576ab3
Jon Kaufman <jhk12345@gmail.com>
2011-08-18 22:05:12 -0400

    initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equity::Application.config.secret_token = 'd57ef7daffec875205111eab137709c8bc641a3caa4198eb45a04384c09d09f8827b253e9546ff53a2377245d01d1b1282e514b827b246b38249d919e6a1f6ff'
