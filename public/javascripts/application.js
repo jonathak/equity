752f32dc6f1de2f4da8d57fefc712f9bc72cf964
Jon Kaufman <jhk12345@gmail.com>
2011-08-19 22:17:02 -0400

    jQuery support for add security view
    displaying only relevant field elements depending on selection of
    security kind.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,27 @@
 
 $(document).ready(function() {
   jQuery("#security_kind").change(function(){ 
-    alert('jQuery is working.');
+	
+    if ($(this).val() == 'c_debt'){
+	  $('.field_liq').fadeIn();
+	  $('.field_debt').fadeIn();
+	  $('.field_cov').fadeIn();
+	  $('.field_pref').fadeOut();
+	}
+	
+	if ($(this).val() == 'preferred'){
+	  $('.field_liq').fadeIn();
+	  $('.field_pref').fadeIn();
+	  $('.field_cov').fadeIn();
+	  $('.field_debt').fadeOut();
+	}	
+	
+	if (($(this).val() == 'common') || ($(this).val() == 'options')){
+	  $('.field_liq').fadeOut();
+	  $('.field_pref').fadeOut();
+	  $('.field_cov').fadeOut();
+	  $('.field_debt').fadeOut();
+	}
+
   });
 });
