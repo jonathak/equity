4a479ade0a99e1edec6e5c3873c3badd62472afb
Jon Kaufman <jhk12345@gmail.com>
2011-08-19 20:11:06 -0400

    added some summary data to dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,9 @@
 class AdminController < ApplicationController
   def home
+    @c_count = Company.count
+    @e_count = Entity.count
+    @s_count = Security.count
+    @t_count = Transaction.count
   end
 
 end
