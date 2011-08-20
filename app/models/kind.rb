b2ab2123045c9b4b40560064760265d33091463f
Jon Kaufman <jhk12345@gmail.com>
2011-08-20 09:02:09 -0400

    just needed to reset kinds

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,6 +1,5 @@
 class Kind < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
-  validates_inclusion_of :name,
-   :in => %w('common', 'preferred', 'c_debt', 'options')
+  validates_inclusion_of :name, :in => %w('common', 'preferred', 'c_debt', 'options')
 end
