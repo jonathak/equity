0a065ce5deed56fc3168614bba58f2f45e7b6952
Jon Kaufman <jhk12345@gmail.com>
2011-08-19 23:20:11 -0400

    just keeping security kinds to the existing list

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,4 +1,6 @@
 class Kind < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
+  validates_inclusion_of :name,
+   :in => %w('common', 'preferred', 'c_debt', 'options')
 end
