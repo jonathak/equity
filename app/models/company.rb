cc2742708edad3a0debbf1750c0292fa8a2e9f30
Jon Kaufman <jhk12345@gmail.com>
2011-08-28 16:18:01 -0400

    created some of the company API for future development of cascades

--- app/models/company.rb
+++ app/models/company.rb
@@ -11,4 +11,21 @@ class Company < ActiveRecord::Base
     investments.map(&:entity_id)
   end
   
+  # id's of companies that company directly invests in
+  def directs
+    alias_ids.map(&:company).map(&:id)
+  end
+  
+  # array of company_id investment chains
+  def chains
+    #need to make this one
+  end
+  
+  # array of company_ids representing linked investors
+  def owners
+    entities.map do |e|
+      e.investment.company.id if e.investment
+    end
+  end
+  
 end
