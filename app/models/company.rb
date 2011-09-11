0de89a9e66ab2976f3df78e8b1b67ab19e6e8fb9
Jon Kaufman <jhk12345@gmail.com>
2011-09-11 12:19:16 -0400

    many people will use "newco"

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
   validates_presence_of :name
-  validates_uniqueness_of :name, :case_sensitive => false
+  # validates_uniqueness_of :name, :case_sensitive => false
   has_many :transactions, :through => :securities
   has_many :securities
   has_many :entities
