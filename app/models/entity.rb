ec7ed14120c9aa2478b1dbacbc7b532b724fa125
Jon Kaufman <jhk12345@gmail.com>
2011-09-15 05:48:34 -0400

    added dollars invested to dashboard

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -6,4 +6,13 @@ class Entity < ActiveRecord::Base
   has_many :buys, :foreign_key => :buyer_id, :class_name => "Transaction"
   has_one :investment
   has_many :requests
+
+  # dollar amount invested less sold (for given security_id (s_id) if passed)
+  def net_dollars(s_id = nil)
+    if s_id
+      buys.where(:security_id => s_id).sum(:dollars) - sales.where(:security_id => s_id).sum(:dollars)
+    else
+      buys.sum(:dollars) - sales.sum(:dollars)
+    end
+  end
 end
