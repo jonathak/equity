f74908ceb8d21bc7b9f4eadef904db631b0d89a5
Jon Kaufman <jhk12345@gmail.com>
2012-02-13 11:37:33 -0500

    needed to be able to purchase shares for zero dollars for stock split.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ActiveRecord::Base
   validates_presence_of :security_id, :seller_id, :buyer_id, :date
   validates_numericality_of :security_id, :seller_id, :buyer_id, {:greater_than => 0, :only_integer => true}
   validates_numericality_of :dollars, :ex_price, {:greater_than => 0, :allow_nil => true}
-  validates_numericality_of :shares, {:greater_than => 0, :allow_nil => true, :only_integer => true}
+  validates_numericality_of :shares, {:greater_than_or_equal_to => 0, :allow_nil => true, :only_integer => true}
   validate :buyer_must_be_different_from_seller
   has_one :company, :through => :security
   belongs_to :seller, :class_name => 'Entity'
